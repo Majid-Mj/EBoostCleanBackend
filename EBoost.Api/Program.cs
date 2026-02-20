f9f7da85c8796063d34dbd3939acf99398631aa1
majid <majidvp501@gmail.com>
2026-02-20 09:28:25 +0530

    Program file again

--- EBoost.Api/Program.cs
+++ EBoost.Api/Program.cs
@@ -1,53 +1,53 @@
-﻿    using EBoost.Api.Extensions;
-    using EBoost.Api.Swagger;
-    using EBoost.Application.Common.Responses;
-    using EBoost.Application.Interfaces.Repositories;
-    using EBoost.Application.Interfaces.Services;
-    using EBoost.Application.Mappings;
-    using EBoost.Application.Services;
-    using EBoost.Infrastructure.Data;
-    using EBoost.Infrastructure.Data.Seed;
-    using EBoost.Infrastructure.Identity;
-    using EBoost.Infrastructure.Repositories;
-    using Microsoft.AspNetCore.Authentication.JwtBearer;
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.IdentityModel.Tokens;
-    using Microsoft.OpenApi.Models;
+﻿using EBoost.Api.Extensions;
+using EBoost.Api.Swagger;
+using EBoost.Application.Common.Responses;
+using EBoost.Application.Interfaces.Repositories;
+using EBoost.Application.Interfaces.Services;
+using EBoost.Application.Mappings;
+using EBoost.Application.Services;
+using EBoost.Infrastructure.Data;
+using EBoost.Infrastructure.Data.Seed;
+using EBoost.Infrastructure.Identity;
+using EBoost.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-    // Add services to the container.
-    builder.Services.AddControllers(options =>
+// Add services to the container.
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<ApiResponseFilter>();
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
     {
-        options.Filters.Add<ApiResponseFilter>();
+        Title = "EBoost API",
+        Version = "v1"
     });
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
 
 
-    builder.Services.AddSwaggerGen(c =>
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        c.SwaggerDoc("v1", new OpenApiInfo
-        {
-            Title = "EBoost API",
-            Version = "v1"
-        });
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer {token}'"
+    });
 
-    
-        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-            Name = "Authorization",
-            Type = SecuritySchemeType.Http,
-            Scheme = "Bearer",
-            BearerFormat = "JWT",
-            In = ParameterLocation.Header,
-            Description = "Enter 'Bearer {token}'"
-        });
-
-        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
         {
             {
                 new OpenApiSecurityScheme
@@ -62,121 +62,123 @@
             }
         });
 
-        var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
-        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-        c.IncludeXmlComments(xmlPath);
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
 
-        c.SchemaFilter<ClearExamplesSchemaFilter>();
-
-
-    });
+    c.SchemaFilter<ClearExamplesSchemaFilter>();
 
 
-    //fro ApiResponces
-    builder.Services.Configure<ApiBehaviorOptions>(options =>
+});
+
+
+//fro ApiResponces
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
     {
-        options.InvalidModelStateResponseFactory = context =>
-        {
-            var errors = context.ModelState
-                .Where(x => x.Value!.Errors.Count > 0)
-                .SelectMany(x => x.Value!.Errors)
-                .Select(e => e.ErrorMessage)
-                .ToList();
+        var errors = context.ModelState
+            .Where(x => x.Value!.Errors.Count > 0)
+            .SelectMany(x => x.Value!.Errors)
+            .Select(e => e.ErrorMessage)
+            .ToList();
 
-            var response = ApiResponse<string>.FailureResponse(
-                "Validation failed",
-                errors
-            );
+        var response = ApiResponse<string>.FailureResponse(
+            "Validation failed",
+            errors
+        );
 
-            return new BadRequestObjectResult(response);
-        };
-    });
+        return new BadRequestObjectResult(response);
+    };
+});
 
 
 
 
-    builder.Services.AddDbContext<EBoostDbContext>(options =>
-        options.UseSqlServer(
-            builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<EBoostDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
-    //builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-    //builder.Services.AddAutoMapper(typeof(ProductProfile).Assembly);
-    builder.Services.AddAutoMapper(typeof(EBoost.Application.Mappings.ProductProfile).Assembly);
+//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//builder.Services.AddAutoMapper(typeof(ProductProfile).Assembly);
+builder.Services.AddAutoMapper(typeof(EBoost.Application.Mappings.ProductProfile).Assembly);
 
 
 
 
-    builder.Services.AddScoped<IPasswordHasher, PasswordHasherService>();
-    builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasherService>();
+builder.Services.AddScoped<AuthService>();
 
-    builder.Services.AddScoped<IUserRepository, UserRepository>();
-    builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
-    builder.Services.AddScoped<ITokenService, TokenService>();
-    builder.Services.AddScoped<IRefreshTokenGenerator, RefreshTokenGenerator>();
-    builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-    builder.Services.AddScoped<IProductRepository, ProductRepository>();
-    builder.Services.AddScoped<IWishlistRepository, WishlistRepository>();
-    builder.Services.AddScoped<ICartRepository, CartRepository>();
-    builder.Services.AddScoped<ICartService, CartService>();
-    builder.Services.AddScoped<ICloudinaryService, CloudinaryService>();
-    builder.Services.AddScoped<IOrderService, OrderService>();
-    builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-    builder.Services.AddScoped<IUserRepository, UserRepository>();
-    builder.Services.AddScoped<IShippingAddressRepository, ShippingAddressRepository>();
-    builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IRefreshTokenGenerator, RefreshTokenGenerator>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IWishlistRepository, WishlistRepository>();
+builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<ICloudinaryService, CloudinaryService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IShippingAddressRepository, ShippingAddressRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IPaymentService, RazorpayPaymentService>();
+
 
 
 
 
 builder.Services.AddJwtAuthentication(builder.Configuration);
 
-    builder.Services.AddAuthorization(options =>
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy =>
+        policy.RequireClaim("roleId", "2"));
+
+    options.AddPolicy("UserOnly", policy =>
+    policy.RequireClaim("roleId", "1"));
+});
+
+
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseMiddleware<ResponseHeaderMiddleware>();
+
+using (var scope = app.Services.CreateScope())
+{
+    try
     {
-        options.AddPolicy("AdminOnly", policy =>
-            policy.RequireClaim("roleId", "2"));
+        var context = scope.ServiceProvider.GetRequiredService<EBoostDbContext>();
+        var hasher = scope.ServiceProvider.GetRequiredService<IPasswordHasher>();
 
-        options.AddPolicy("UserOnly", policy =>
-        policy.RequireClaim("roleId", "1"));
-    });
-
-
-    var app = builder.Build();
-
-    app.UseMiddleware<ExceptionMiddleware>();
-    app.UseMiddleware<ResponseHeaderMiddleware>();
-
-    using (var scope = app.Services.CreateScope())
-    {
-        try
-        {
-            var context = scope.ServiceProvider.GetRequiredService<EBoostDbContext>();
-            var hasher = scope.ServiceProvider.GetRequiredService<IPasswordHasher>();
-
-            await AdminSeeder.SeedAsync(context, hasher);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Admin seeding failed:");
-            Console.WriteLine(ex.Message);
-        }
+        await AdminSeeder.SeedAsync(context, hasher);
     }
-
-
-
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
+    catch (Exception ex)
     {
-        app.UseSwagger();
-        app.UseSwaggerUI();
+        Console.WriteLine("Admin seeding failed:");
+        Console.WriteLine(ex.Message);
     }
+}
 
-    app.UseHttpsRedirection();
 
-    app.UseAuthentication();
-    app.UseAuthorization();
 
-    app.MapControllers();
 
-    app.Run();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
