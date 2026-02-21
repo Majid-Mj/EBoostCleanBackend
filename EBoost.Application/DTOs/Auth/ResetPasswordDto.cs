930dad2d35cab7ccb715d439b5f8fe1821214871
majid <majidvp501@gmail.com>
2026-02-21 08:46:17 +0530

    Validation added for ResetPasswordDto

--- EBoost.Application/DTOs/Auth/ResetPasswordDto.cs
+++ EBoost.Application/DTOs/Auth/ResetPasswordDto.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace EBoost.Application.DTOs.Auth;
 
 public class ResetPasswordDto
 {
-    public string Email { get; set; }
-    public string Otp { get; set; }
-    public string NewPassword { get; set; }
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    [MaxLength(256)]
+    public string Email { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "OTP is required.")]
+    [RegularExpression(@"^\d{6}$",
+        ErrorMessage = "OTP must be a 6-digit number.")]
+    public string Otp { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "New password is required.")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")]
+    [MaxLength(100, ErrorMessage = "Password cannot exceed 100 characters.")]
+    [RegularExpression(
+        @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$",
+        ErrorMessage = "Password must contain uppercase, lowercase, number, and special character."
+    )]
+    public string NewPassword { get; set; } = string.Empty;
 }
